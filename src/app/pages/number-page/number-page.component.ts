1a0f4b6b7726fcbb65136a60cf5f016b312fcd51
emiliano.lopez <emiliano.lopez@LAP-EMILIANO>
2026-09-21 14:32:02 -0700

    pipes básicos y numéricos

--- src/app/pages/number-page/number-page.component.ts
+++ src/app/pages/number-page/number-page.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-number-page',
-  imports: [],
+  imports: [DecimalPipe, CurrencyPipe, PercentPipe],
   templateUrl: './number-page.component.html',
 })
-export default class NumberPageComponent {}
+export default class NumberPageComponent {
+  totalSales = signal(2_459_807.5567);
+  percent = signal(0.4856);
+}

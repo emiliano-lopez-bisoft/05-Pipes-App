ca3680c130b34456830b4d740eb1a8f4a2f76d7b
emiliano.lopez <emiliano.lopez@LAP-EMILIANO>
2026-09-21 12:23:45 -0700

    Primer commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('05-pipes-app');
+  protected readonly title = signal('pipes-app');
 }

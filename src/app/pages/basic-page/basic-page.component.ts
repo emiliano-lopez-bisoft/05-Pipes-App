4ca851c59bbedabbe240af6e6df70eb6d55727ba
emiliano.lopez <emiliano.lopez@LAP-EMILIANO>
2026-09-23 08:29:04 -0700

    cambiar idiomas

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,6 @@
 import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, effect, signal } from '@angular/core';
+import { Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+import { AvailableLocale, LocaleService } from '../../services/locale.service';
 
 @Component({
   selector: 'app-basic-page',
@@ -7,6 +8,9 @@ import { Component, effect, signal } from '@angular/core';
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
+  localeService = inject(LocaleService);
+  currentLocale = signal(inject(LOCALE_ID));
+
   nameLower = signal('emiliano');
   nameUpper = signal('EMILIANO');
   fullName = signal('emIliAnO lOpeZ');
@@ -22,4 +26,8 @@ export default class BasicPageComponent {
       clearInterval(interval);
     });
   });
+
+  changeLocale(locale: AvailableLocale) {
+    this.localeService.changeLocale(locale);
+  }
 }
